39132bac39780b6b05a43140844f2e7a9c9c440f
HP laptop <ark1111.khan@gmail.com>
2024-08-15 08:09:14 +0500

    AI first commit

---
+++ app/src/main/java/maan/quiteasy/suggestionsfromgemini/MainActivity.kt
@@ -0,0 +1,28 @@
+package maan.quiteasy.suggestionsfromgemini
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.ui.Modifier
+import maan.quiteasy.suggestionsfromgemini.ui.theme.SuggestionsFromGeminiTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            SuggestionsFromGeminiTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background,
+                ) {
+                    BakingScreen()
+                    //GalleryImagePicker()
+                }
+            }
+        }
+    }
+}
---
+++ app/src/main/java/maan/quiteasy/suggestionsfromgemini/SuggestionScreen.kt
@@ -0,0 +1,146 @@
+package maan.quiteasy.suggestionsfromgemini
+
+import android.graphics.BitmapFactory
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material3.Button
+import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
+import androidx.lifecycle.viewmodel.compose.viewModel
+import android.net.Uri
+import androidx.activity.compose.rememberLauncherForActivityResult
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.colorResource
+import coil.compose.AsyncImage
+
+@Composable
+fun BakingScreen(
+    suggestionsViewModel: SuggestionsViewModel = viewModel()
+) {
+    var imageUri by remember {
+        mutableStateOf<Uri?>(null)
+    }
+    val launcher = rememberLauncherForActivityResult(
+        contract =
+        ActivityResultContracts.GetContent()
+    ) { uri: Uri? ->
+        imageUri = uri
+    }
+
+    val placeholderResult = stringResource(R.string.results_placeholder)
+    var prompt by rememberSaveable { mutableStateOf("") }
+    var result by rememberSaveable { mutableStateOf(placeholderResult) }
+    val uiState by suggestionsViewModel.uiState.collectAsState()
+    val context = LocalContext.current
+
+    Column(
+        modifier = Modifier.fillMaxSize()
+    ) {
+        Text(
+            text = stringResource(R.string.baking_title),
+            style = MaterialTheme.typography.titleLarge,
+            modifier = Modifier.padding(16.dp)
+        )
+
+        AsyncImage(
+            model = if(imageUri == null){
+                R.drawable.plus
+            }else{
+                imageUri
+            },
+            contentDescription = null,
+            modifier = Modifier
+                .padding(4.dp)
+                .fillMaxWidth()
+                .height(200.dp)
+                .clip(RoundedCornerShape(12.dp))
+                .background(color = colorResource(id = R.color.teal_200))
+                .clickable {
+                    launcher.launch("image/*")
+                },
+            contentScale = if(imageUri == null){
+                ContentScale.Fit
+            }else{
+                ContentScale.FillBounds
+            }
+        )
+
+
+        Row(
+            modifier = Modifier.padding(all = 16.dp)
+        ) {
+            TextField(
+                value = prompt,
+                label = { Text(stringResource(R.string.label_prompt)) },
+                onValueChange = { prompt = it },
+                modifier = Modifier
+                    .weight(0.8f)
+                    .padding(end = 16.dp)
+                    .align(Alignment.CenterVertically)
+            )
+
+            Button(
+                onClick = {
+                    val inputStream = imageUri?.let { context.contentResolver.openInputStream(it) }
+                    val bitmap = BitmapFactory.decodeStream(inputStream)
+                    suggestionsViewModel.sendPrompt(bitmap, prompt)
+                },
+                enabled = prompt.isNotEmpty(),
+                modifier = Modifier
+                    .align(Alignment.CenterVertically)
+            ) {
+                Text(text = stringResource(R.string.action_go))
+            }
+        }
+
+        if (uiState is UiState.Loading) {
+            CircularProgressIndicator(modifier = Modifier.align(Alignment.CenterHorizontally))
+        } else {
+            var textColor = MaterialTheme.colorScheme.onSurface
+            if (uiState is UiState.Error) {
+                textColor = MaterialTheme.colorScheme.error
+                result = (uiState as UiState.Error).errorMessage
+            } else if (uiState is UiState.Success) {
+                textColor = MaterialTheme.colorScheme.onSurface
+                result = (uiState as UiState.Success).outputText
+            }
+            val scrollState = rememberScrollState()
+            Text(
+                text = result,
+                textAlign = TextAlign.Start,
+                color = textColor,
+                modifier = Modifier
+                    .align(Alignment.CenterHorizontally)
+                    .padding(16.dp)
+                    .fillMaxSize()
+                    .verticalScroll(scrollState)
+            )
+        }
+    }
+}
